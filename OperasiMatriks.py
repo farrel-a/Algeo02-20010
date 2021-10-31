cfe2380249f5f6fbaf6bc2ef0119e127f6cdd96c
Farrel Ahmad <13520110@std.stei.itb.ac.id>
2021-10-31 08:04:13 +0700

    Update eigenFinderMxN function, still limited to 2x2 AAt

--- OperasiMatriks.py
+++ OperasiMatriks.py
@@ -60,6 +60,56 @@ def substractMat(m1,m2):    #m1-m2
 
     return res
 
+#modified cofactor determinant with element as equation
+def modifiedDetCof(m):
+    row = len(m)
+    col = len(m[0])
+    if (row==1 and col==1):   #base 1
+        return m
+    elif (row==2 and col==2): #base 2
+        max_degree1 = (len(m[0][0])-1) + (len(m[1][1])-1)
+        max_degree2 = (len(m[0][1])-1) + (len(m[1][0])-1)
+        if (max_degree1 > max_degree2):
+            arr = [0 for i in range(max_degree1+1)]
+        else:
+            arr = [0 for i in range(max_degree2+1)]
+        #[0][0] * [1][1]
+        for i in range(len(m[0][0])):
+            for j in range(len(m[1][1])):
+                arr[i+j] += m[0][0][i]*m[1][1][j]
+        
+        #[0][1] * [1][0]
+        for i in range (len(m[0][1])):
+            for j in range(len(m[1][0])):
+                arr[i+j] = arr[i+j] - (m[0][1][i]*m[1][0][j])
+        return arr
+    else:   #recursive
+        print()
+        #in progress
+
+#solve equation, input array =  [x^0, x^1, x^2,...]
+def solveEquation(arr):
+    if len(arr)==1:
+        return arr[0]
+    elif len(arr)==2: #[x^0,x^1] 
+        return arr[1]
+    elif len(arr)==3: #[x^0,x^1,x^2]
+        D = (arr[1]**2)-(4*arr[2]*arr[0]) #discriminant
+        if (D<0):
+            return []
+        else:
+            solution = []
+            sqrtD = D**0.5
+            s1 = (-arr[1]+sqrtD)/(2*arr[2])
+            solution.append(s1)
+            s2  = (-arr[1]-sqrtD)/(2*arr[2])
+            solution.append(s2)
+            return solution
+    else:
+        print()
+        #in progressm, higher nth degree polynomial
+
+
 def eigenFinderMxN(m):
     A = m
     At = transpose(m)
@@ -67,7 +117,8 @@ def eigenFinderMxN(m):
     row = len(AAt)
     col = row
 
-    LambdaIminusA = [[[0,0] for j in range(col)]for i in range(row)] #[[lambda^0,lambda^1,lambda^2,...], konstanta]
+    #[lambda^0,lambda^1,lambda^2,...]
+    LambdaIminusA = [[[0,0] for j in range(col)]for i in range(row)]
     for i in range(row):
         for j in range(col):
             if (i==j):
@@ -76,5 +127,7 @@ def eigenFinderMxN(m):
             else:
                 LambdaIminusA[i][j][1] = 0
                 LambdaIminusA[i][j][0] = -AAt[i][j]
-    print(LambdaIminusA)
-    #in progress
+    det = modifiedDetCof(LambdaIminusA)
+    sol = solveEquation(det)
+    return sorted(sol, reverse=True)
+    
--- test.py
+++ test.py
@@ -4,4 +4,5 @@
 from OperasiMatriks import eigenFinderMxN
 
 m = [[3,1,1],[-1,3,1]]
-eigenFinderMxN(m)
+arr = eigenFinderMxN(m)
+print(arr)
