003fc4d63fc041a54a9089c500999863e6dcfc47
geraldakbar <mgeraldakbar@gmail.com>
2021-11-09 12:19:28 +0700

    gambar bisa upload ke folder upload, blm bisa di display

--- Website/__init__.py
+++ Website/__init__.py
@@ -1,6 +1,6 @@
-from flask import Flask,render_template,request,flash,redirect,url_for
+from flask import Flask, render_template, request, flash, redirect, url_for
 import urllib.request
-import  os
+import os
 from werkzeug.utils import secure_filename
 
 
@@ -8,10 +8,11 @@ from werkzeug.utils import secure_filename
 def create_app():
     app = Flask(__name__)
 
-    UPLOAD_FOLDER = 'static/uploads'
+    UPLOAD_FOLDER = os.getcwd() + "/Website/static/uploads"
     app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+    app.config["SECRET_KEY"] = 'Semangat ges'
 
-    ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+    ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
     def allowed_file(filename):
         return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -19,27 +20,29 @@ def create_app():
     @app.route('/')
     @app.route("/home")
     def home():
-        #Nanti fungsi SVD Bakal ada disini
+        # Nanti fungsi SVD Bakal ada disini
         return render_template("index.html")
 
-    @app.route("/",methods = ["POST","GET"])
+    @app.route("/", methods=["POST", "GET"])
     def display_converted_image():
-        if 'file' not in request.files:
-            flash("No file detected")
-            return redirect(url_for('home'))
-        file = request.files['file']
-        if file.filename == "":
-            flash("No image selected")
-            return (request.url)
+        if request.method == "POST":
+            if 'file' not in request.files:
+                flash("No file detected")
+                return redirect(url_for('home'))
+            file = request.files['file']
+            if file.filename == "":
+                flash("No image selected")
+                return request.url
 
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-            flash('Image sucsessfully uploaded and displayed')
-            return (render_template('index.html', filename=filename))
-        else:
-            flash("Allowed image type are: png,jpg, or jpeg")
-            return redirect(request.url)
+            if file and allowed_file(file.filename):
+                filename = file.filename
+                file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+                flash('Image sucsessfully uploaded and displayed')
+                return render_template('index.html', filename=filename,img_src=UPLOAD_FOLDER + filename)
+
+            else:
+                flash("Allowed image type are: png,jpg, or jpeg")
+                return redirect(request.url)
 
 
     return app
