2b8bb7a433ac2fcdfbb288efb6b43156c7ca5d92
geraldakbar <mgeraldakbar@gmail.com>
2021-11-14 23:48:54 +0700

    show runtime  & fixed path error for windows

--- src/Website/__init__.py
+++ src/Website/__init__.py
@@ -37,15 +37,16 @@ def create_app():
                 filename = file.filename
                 ratio = int(request.form.get('ratio'))
                 path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+                path = os.path.abspath(path)
                 print(filename)
                 file.save(path)
                 fn = os.path.splitext(path)
                 root,ext = os.path.splitext(path)
                 compressed_filename = Path(fn[0] + "_compressed").stem + ext
-                compressImg(path,ratio)
+                runtime = compressImg(path,ratio)
                 print(compressed_filename)
                 flash('Image sucsessfully uploaded and displayed')
-                return render_template('index.html', filename=filename,com_fn = compressed_filename)
+                return render_template('index.html', filename=filename,com_fn = compressed_filename,rt = runtime,rat=ratio)
 
             else:
                 flash("Allowed image type are: png,jpg, or jpeg")
@@ -54,10 +55,14 @@ def create_app():
     @app.route('/display/<filename>')
     def display_image(filename):
         # print('display_image filename: ' + filename)
+        filename = 'uploads/'+filename
+        filename = os.path.abspath(filename)
         return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
     @app.route('/display/<filename>')
     def display_compressed_image(filename):
+        filename = 'uploads/' + filename
+        filename = os.path.abspath(filename)
         return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 
